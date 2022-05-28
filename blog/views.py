f83cc042e2dad53c1e4a3a4f8d6e7638f95bce7a
unknown <susan@gmail.com>
2022-05-28 01:41:59 -0700

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,41 @@
+from datetime import timezone
+from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
-
+from .forms import PostForm
 from blog.models import Post
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+     return render(request, 'blog/post_list.html', {})
 def post_detail(request, pk):
+    Post.objects.get(pk=pk)
     post = get_object_or_404(Post, pk=pk)
+    return redirect('post_detail', pk=post.pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save() 
+            return redirect('post_detail', pk=post.pk) 
+    else:
+         form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})   
+
+
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bb=i#^y7r=z6puvz%u4j$#%a3*o1acvoe4(d7d(p1zt+@)n=y^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
